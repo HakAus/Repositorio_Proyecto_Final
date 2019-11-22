a0278f42a077c54eda7aef41da2398c2ec0788d3
Xfighter32 <austinhakanson2@gmail.com>
2019-11-22 13:07:23 -0600

    En el sp de cargar datos se agrego lo necesario para cargar salarioxhora y los tipos de deduccion por herencia (fija o porcentual), en el sp de reiniciar tablas se incluye salarioxhora

--- Stored Procedures/SP_ReiniciarTablas.sql
+++ Stored Procedures/SP_ReiniciarTablas.sql
@@ -1,40 +1,10 @@
 USE [Proyecto]
 GO
 
-/****** Object:  StoredProcedure [dbo].[ReiniciarTablas]    Script Date: 11/22/2019 7:45:42 AM ******/
-SET ANSI_NULLS ON
+DECLARE	@return_value int
+
+EXEC	@return_value = [dbo].[ReiniciarTablas]
+
+SELECT	'Return Value' = @return_value
+
 GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
--- ================================================================================================
--- Autores:		<Austin Hakanson y Antony Artavia>
--- Fecha de creacion: <21-11-2019>
--- Fecha de Ultima Actualizacion: <21-11-2019>
--- Descripcion:	SP para eliminar todos los registros de las tablas y reiniciar los contadores
--- ================================================================================================
-CREATE OR ALTER PROCEDURE [dbo].[ReiniciarTablas]
-
-AS
-BEGIN
-	SET NOCOUNT ON;
-
-    -- /// SCRIPT PARA REINICIAR LAS TABLAS ///
-
-	DELETE FROM [Proyecto].[dbo].[Puesto]
-	DBCC CHECKIDENT ('Puesto', RESEED, 0)
-
-	DELETE FROM [Proyecto].[dbo].Feriado
-	DBCC CHECKIDENT ('Feriado', RESEED, 0)
-
-	DELETE FROM [Proyecto].[dbo].TipoJornada
-	DBCC CHECKIDENT ('TipoJornada', RESEED, 0)
-
-	DELETE FROM [Proyecto].[dbo].[TipoMovPlanilla]
-	DBCC CHECKIDENT ('TipoMovPlanilla', RESEED, 0)
-
-
-END
-GO
-
